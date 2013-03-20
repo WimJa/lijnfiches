bd4e7db9e0c3453578d3ebf9b2e0a96b72d40daa
evert <beattls@gmail.com>
2013-03-20 18:36:57 +0100

    Quotes voor spaties en xarg

--- automate.sh
+++ automate.sh
@@ -3,6 +3,6 @@
 # Replace all default 'Sans' font (on windows Arial, on linux Dejavu sans) with Arial
 # This fixes the rendering differences on win vs linux when there is no font defined ('Sans')
 
-find . -name "*.svg" -print | xargs sed -i 's/font-family:Sans/font-family:Arial/g'
+find . -name "*.svg" -print  | sed 's/.*/"&"/' | xargs sed -i 's/font-family:Sans/font-family:Arial/g'
 
 
