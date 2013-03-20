00484bac58a7bbedc046c305b930ef5cdaed9388
evert <beattls@gmail.com>
2013-03-20 18:32:54 +0100

    Script om font automagisch te veranderen

---
+++ automate.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Replace all default 'Sans' font (on windows Arial, on linux Dejavu sans) with Arial
+# This fixes the rendering differences on win vs linux when there is no font defined ('Sans')
+
+find . -name "*.svg" -print | xargs sed -i 's/font-family:Sans/font-family:Arial/g'
+
+
