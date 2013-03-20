c27817464581c85291dc35aa94bb98059ccd2319
evert <beattls@gmail.com>
2013-03-20 19:24:42 +0100

    big pdf ftw

--- automate.sh
+++ automate.sh
@@ -9,4 +9,7 @@ find . -name "*.svg" -print  | sed 's/.*/"&"/' | xargs sed -i 's/font-family:San
 # generate pdf's of all svg's :)
 find . -name "*svg" -exec inkscape {} --export-pdf=pdf/{}.pdf \;
 
-
+# and for the fun and printing one BIG pdf containing all the goodies
+pdfunite pdf/A4* pdf/FULL_PRINT_A4.pdf
+# only one A5 yet
+#pdfunite pdf/L* pdf/FULL_PRINT_A5.pdf
